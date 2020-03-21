6d2d1d7e06339cfe50358286610d92ae50daf325
Dan Eden <deden@elogicgroup.com>
2020-03-21 13:26:50 -0400

    Delirium changes

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+
 var express = require('express');
 var app = express();
 
@@ -8,6 +10,28 @@ app.get('/', function(req, res,) {
 
 });
 
+app.get(`/stash/:league/:username/:stashNumber`, async (req, res,) => {
+    try {
+        var data =  await axios.get(
+            `https://www.pathofexile.com/character-window/get-stash-items?league=${req.params.league}&tabs=1&tabIndex=${req.params.stashNumber}&accountName=${req.params.username}`,
+            {
+                headers: {
+                    cookie: "_ga=GA1.2.1614616722.1560806896; stored_data=1; POESESSID=36d889f906d82dbe181acca0bdb4602b; __cfduid=d765415bb96aba231e044cb202fe3c3d21584064036"
+                }
+            }
+        )
+        res.send(data.data);
+    } catch (error) {
+        console.error(error)
+        res.send(error);
+    }
+
+    
+});
+
+
+
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
+
--- public/js/stash.js
+++ public/js/stash.js
@@ -1,14 +1,21 @@
+const stash = {
+    league: `Delirium`,
+    userName: `edenmind`,
+    stashNUmber: 5,
+    baseUrl: "http://localhost:3000"
+}
+
 /**
  * Function to set the stash grid overlay
  */
 function SetStashUrl() {
     // must be logged into PoE with username
-    const userName = `edenmind`;
-    const league = `Delirium`;
-    const stashNUmber = 5; // Stash numbers start at 0
-    const myStashUrl = `https://www.pathofexile.com/character-window/get-stash-items?league=${league}&tabs=1&tabIndex=${stashNUmber}&accountName=${userName}`;
-    // set href on UI
-    document.getElementById('myStashUrl').href = myStashUrl;
+    // const userName = `edenmind`;
+    // const league = `Delirium`;
+    // const stashNUmber = 5; // Stash numbers start at 0
+    // const myStashUrl = `https://www.pathofexile.com/character-window/get-stash-items?league=${league}&tabs=1&tabIndex=${stashNUmber}&accountName=${userName}`;
+    // // set href on UI
+    // document.getElementById('myStashUrl').href = myStashUrl;
 }
 
 /**
@@ -16,47 +23,53 @@ function SetStashUrl() {
  * Triggers on Import Stash button confirmation
  */
 document.getElementById('importStashButton').onclick = function() {
-    ClearGrid(false, true);
+    ClearGrid(true, true);
     // Get stash items from my logged-in PoE stash
-    var responseText = document.getElementById("stashTextArea").value;
-    var responseObj = JSON.parse(responseText);
-    var items = responseObj.items;
-    var tab = responseObj.tabs;
+    // var responseText = document.getElementById("stashTextArea").value;
 
-    console.log(items);
+    var responseText = $.get(`${stash.baseUrl}/stash/${stash.league}/${stash.userName}/${stash.stashNUmber}`, function( data ) {
+        //$( ".result" ).html( data );
+        console.log(data);
 
-    for(var i = 0; i < items.length; i++) {
-        // get item data
-        var url = items[i].icon;
-        var y = items[i].y;
-        var x = items[i].x;
-        const league = items[i].league;
-        const name = items[i].name;
-        const typeLine = items[i].typeLine;
-
-        // set image to input on grid
-        var cellImages = document.getElementsByClassName("cellInput");
-        for(var j = 0; j < cellImages.length; j++) {
-            if(cellImages[j].getAttribute('y') === `${y}` && cellImages[j].getAttribute('x') === `${x}`) {
-                // set image icon
-                cellImages[j].style.backgroundImage = `url("${url}")`;
-
-                // set price
-                cellImages[j].value = (items[i].note).split("~price ")[1];
-
-                // set tooltip
-                var itemToolTip = document.createElement("span");
-                itemToolTip.className = (`tooltiptext`);
-                var separator = ` - `;
-                if(name == "" || typeLine == "") {
-                    var separator = ``;
+        var items = data.items;
+        var tab = data.tabs;
+    
+        for(var i = 0; i < items.length; i++) {
+            // get item data
+            var url = items[i].icon;
+            var y = items[i].y;
+            var x = items[i].x;
+            const league = items[i].league;
+            const name = items[i].name;
+            const typeLine = items[i].typeLine;
+    
+            // set image to input on grid
+            var cellImages = document.getElementsByClassName("cellInput");
+            for(var j = 0; j < cellImages.length; j++) {
+                if(cellImages[j].getAttribute('y') === `${y}` && cellImages[j].getAttribute('x') === `${x}`) {
+                    // set image icon
+                    cellImages[j].style.backgroundImage = `url("${url}")`;
+    
+                    // set price
+                    if(items[i].note) {
+                        cellImages[j].value = (items[i].note).replace("~price ", "");
+                    }
+                    
+    
+                    // set tooltip
+                    var itemToolTip = document.createElement("span");
+                    itemToolTip.className = (`tooltiptext`);
+                    var separator = ` - `;
+                    if(name == "" || typeLine == "") {
+                        var separator = ``;
+                    }
+                    itemToolTip.innerHTML = (`${name}${separator}${typeLine}`);
+                    cellImages[j].parentElement.appendChild(itemToolTip);
                 }
-                itemToolTip.innerHTML = (`${name}${separator}${typeLine}`);
-                cellImages[j].parentElement.appendChild(itemToolTip);
             }
         }
-    }
-    SetStashPricePost();
+        SetStashPricePost();
+    });
 }
 
 
@@ -64,8 +77,6 @@ document.getElementById('importStashButton').onclick = function() {
  * Function to get and display the forum post
  */
 function SetStashPricePost() {
-
-	const league = `Delirium`;
 	const cellInput = document.getElementsByClassName('cellInput');
 	var postText = [];
 
@@ -76,7 +87,7 @@ function SetStashPricePost() {
 			const y = cellInput[i].getAttribute("y");
 			const x = cellInput[i].getAttribute("x");
 			
-			postText.push(`[linkItem location="Stash6" league="${league}" x="${x}" y="${y}"]`);
+			postText.push(`[linkItem location="Stash6" league="${stash.league}" x="${x}" y="${y}"]`);
 			postText.push(`~b/o ${thisInputValue}`);
 		}
 	}
